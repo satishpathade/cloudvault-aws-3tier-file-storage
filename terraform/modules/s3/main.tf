b59ae3743fa83dc8c0c8a9807472a41379f1504e
satish <pathadesatish0@gmail.com>
2026-05-16 20:35:02 +0530

    change s3 public access

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -1,5 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-file-storage"
+  force_destroy = true
 
   tags = {
     project = var.project_name
@@ -29,15 +30,15 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
   }
 }
 
-# Block public access
+# Block all public access
 
 resource "aws_s3_bucket_public_access_block" "this" {
   bucket = aws_s3_bucket.this.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 # CORS configuration
@@ -60,31 +61,4 @@ resource "aws_s3_bucket_cors_configuration" "this" {
 
     max_age_seconds = 3000
   }
-}
-
-# Bucket policy for public read access
-
-resource "aws_s3_bucket_policy" "this" {
-  bucket = aws_s3_bucket.this.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-
-    Statement = [
-      {
-        Sid    = "PublicReadGetObject"
-        Effect = "Allow"
-
-        Principal = "*"
-
-        Action = [
-          "s3:GetObject"
-        ]
-
-        Resource = [
-          "${aws_s3_bucket.this.arn}/*"
-        ]
-      }
-    ]
-  })
 }
