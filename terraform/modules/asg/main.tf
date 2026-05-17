95aaec280d28bce7220f3cb73cf4a264317e67fe
satish <pathadesatish0@gmail.com>
2026-05-17 12:17:10 +0530

    chnage server tags

--- terraform/modules/asg/main.tf
+++ terraform/modules/asg/main.tf
@@ -34,26 +34,31 @@ resource "aws_launch_template" "web" {
     resource_type = "instance"
 
     tags = {
-      Name = "${var.project_name}-web-server"
+      Name    = "${var.project_name}-web-server"
       Project = var.project_name
     }
   }
 }
 
 # Web ASG
-
 resource "aws_autoscaling_group" "web" {
-  name                = "${var.project_name}-web-asg"
-  desired_capacity    = var.web_desired_size
-  min_size            = var.web_min_size
-  max_size            = var.web_max_size
-  force_delete = true
+  name             = "${var.project_name}-web-asg"
+  desired_capacity = var.web_desired_size
+  min_size         = var.web_min_size
+  max_size         = var.web_max_size
+  force_delete     = true
 
   vpc_zone_identifier = var.web_subnet_ids
 
   launch_template {
     id      = aws_launch_template.web.id
     version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-web-server"
+    propagate_at_launch = true
   }
 
   tag {
@@ -84,26 +89,31 @@ resource "aws_launch_template" "app" {
     resource_type = "instance"
 
     tags = {
-      Name = "${var.project_name}-app-server"
+      Name    = "${var.project_name}-app-server"
       Project = var.project_name
     }
   }
 }
 
 # App ASG
-
 resource "aws_autoscaling_group" "app" {
-  name                = "${var.project_name}-app-asg"
-  desired_capacity    = var.app_desired_size
-  min_size            = var.app_min_size
-  max_size            = var.app_max_size
-  force_delete = true
+  name             = "${var.project_name}-app-asg"
+  desired_capacity = var.app_desired_size
+  min_size         = var.app_min_size
+  max_size         = var.app_max_size
+  force_delete     = true
 
   vpc_zone_identifier = var.app_subnet_ids
 
   launch_template {
     id      = aws_launch_template.app.id
     version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-app-server"
+    propagate_at_launch = true
   }
 
   tag {
