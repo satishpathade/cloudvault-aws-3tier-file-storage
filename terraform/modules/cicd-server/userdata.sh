00aac78d6e2336175ffda0dc7389d7608f38eb6c
satish <pathadesatish0@gmail.com>
2026-05-17 12:48:13 +0530

    install ansible using userdata script

---
+++ terraform/modules/cicd-server/userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo dnf update -y
+sudo dnf install git -y
+sudo dnf install python3-pip -y
+sudo dnf install ansible -y
+pip3 install boto3 botocore
+ansible-galaxy collection install amazon.aws
