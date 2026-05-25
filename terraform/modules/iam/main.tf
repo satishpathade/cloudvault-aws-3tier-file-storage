63a450cbc8168a20bf80ad6bcb51eaeaf3d4991f
satish <pathadesatish0@gmail.com>
2026-05-25 21:21:27 +0530

    update ec2 iam role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -6,8 +6,8 @@ resource "aws_iam_role" "ec2_role" {
 
     Statement = [
       {
-        Action = "sts:AssumeRole"
         Effect = "Allow"
+        Action = "sts:AssumeRole"
 
         Principal = {
           Service = "ec2.amazonaws.com"
@@ -19,10 +19,39 @@ resource "aws_iam_role" "ec2_role" {
   tags = var.tags
 }
 
+# CloudWatch Agent
+
 resource "aws_iam_role_policy_attachment" "cloudwatch_agent" {
   role       = aws_iam_role.ec2_role.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
+
+# Ansible Dynamic Inventory Permissions
+
+resource "aws_iam_role_policy" "ansible_inventory" {
+  name = "${var.project_name}-ansible-inventory"
+  role = aws_iam_role.ec2_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeTags",
+          "ec2:DescribeRegions",
+          "ec2:DescribeAvailabilityZones"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+# EC2 Instance Profile
 
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "${var.project_name}-iam"
