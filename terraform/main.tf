98f4f01d25120d21cf687be7c77a51b7ef53bb2c
satish <pathadesatish0@gmail.com>
2026-05-25 21:20:17 +0530

    update main.tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,12 +74,12 @@ module "asg" {
 }
 
 module "cicd_server" {
-  source = "./modules/cicd-server"
-  project_name = var.project_name
-  vpc_id           = module.vpc.vpc_id
-  public_subnet_id = module.vpc.public_subnet_ids[0]
-  cicd_instance_type = var.cicd_instance_type
-  key_name          = var.key_name
+  source                = "./modules/cicd-server"
+  project_name          = var.project_name
+  vpc_id                = module.vpc.vpc_id
+  public_subnet_id      = module.vpc.public_subnet_ids[0]
+  cicd_instance_type    = var.cicd_instance_type
+  key_name              = var.key_name
   instance_profile_name = module.iam.instance_profile_name
 }
 
