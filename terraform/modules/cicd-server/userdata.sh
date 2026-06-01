1d30179bd398de6706e4630c14d6c8d4f13fb240
satish <pathadesatish0@gmail.com>
2026-06-01 23:23:43 +0530

    update cicd server userdata script

--- terraform/modules/cicd-server/userdata.sh
+++ terraform/modules/cicd-server/userdata.sh
@@ -7,3 +7,14 @@ sudo dnf install python3-pip -y
 sudo dnf install ansible -y
 pip3 install boto3 botocore
 ansible-galaxy collection install amazon.aws
+
+mkdir -p /home/ec2-user/.ssh
+
+aws s3 cp \
+s3://cloudvault-file-storage/cloudvault-cicd.pem \
+/home/ec2-user/.ssh/cloudvault-cicd.pem
+
+chmod 400 /home/ec2-user/.ssh/cloudvault-cicd.pem
+
+chown ec2-user:ec2-user \
+/home/ec2-user/.ssh/cloudvault-cicd.pem
