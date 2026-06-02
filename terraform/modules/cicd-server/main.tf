2653fff5e7a80f48ca58cf6aa5069b9b54496f9b
satish <pathadesatish0@gmail.com>
2026-06-02 23:56:17 +0530

    update security group rule

--- terraform/modules/cicd-server/main.tf
+++ terraform/modules/cicd-server/main.tf
@@ -49,13 +49,13 @@ resource "aws_security_group" "cicd" {
   }
 
   # k8s API port
-  ingress {
-    description = "kubernetes API"
-    from_port = 6443
-    to_port = 6443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  } 
+ ingress {
+  description = "Kubernetes API"
+  from_port   = 6443
+  to_port     = 6443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
 
   # kubelet port
   ingress {
