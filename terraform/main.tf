a5a83dd8613d246b4d245f728210fa7382d4105c
satish <pathadesatish0@gmail.com>
2026-06-03 00:07:07 +0530

    update security group modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ module "security_groups" {
   source = "./modules/security-groups"
   project_name = var.project_name
   vpc_id       = module.vpc.vpc_id
-  cicd_sg_id   = module.cicd_server.cicd_sg_id
+  cicd_sg_id = module.cicd_server.cicd_sg_id
   tags         = var.tags
 }
 
