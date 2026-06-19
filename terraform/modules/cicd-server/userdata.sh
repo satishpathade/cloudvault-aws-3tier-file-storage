cae452edc1ba31d65c978757ca75bc8ded2a80e0
satish <pathadesatish0@gmail.com>
2026-06-19 15:30:19 +0530

    private key for jenkins user

--- terraform/modules/cicd-server/userdata.sh
+++ terraform/modules/cicd-server/userdata.sh
@@ -5,8 +5,6 @@ sudo dnf install git tree python3-pip ansible -y
 pip3 install boto3 botocore
 ansible-galaxy collection install amazon.aws
 
-cd /home/ec2-user
-sudo -u ec2-user git clone https://github.com/satishpathade/cloudvault-aws-3tier-devsecops.git
 
 sudo xfs_growfs -d /
 
@@ -16,6 +14,12 @@ aws s3 cp \
 s3://cloudvault-file-storage/cloudvault-cicd.pem \
 /home/ec2-user/.ssh/cloudvault-cicd.pem
 
+# ec2-user
 sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/cloudvault-cicd.pem
 sudo chmod 400 /home/ec2-user/.ssh/cloudvault-cicd.pem
-sudo chmod -R 755 /home/ec2-user/cloudvault-aws-3tier-devsecops
+
+# jenkins user
+mkdir -p /var/lib/jenkins/.ssh
+cp /home/ec2-user/.ssh/cloudvault-cicd.pem /var/lib/jenkins/.ssh/cloudvault-cicd.pem
+chown jenkins:jenkins /var/lib/jenkins/.ssh/cloudvault-cicd.pem
+chmod 400 /var/lib/jenkins/.ssh/cloudvault-cicd.pem
