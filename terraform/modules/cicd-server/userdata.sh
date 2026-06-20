6947fdc6f9dea7f767ff610092612ef96260728d
satish <pathadesatish0@gmail.com>
2026-06-20 23:18:22 +0530

    remove private key copy task

--- terraform/modules/cicd-server/userdata.sh
+++ terraform/modules/cicd-server/userdata.sh
@@ -17,9 +17,3 @@ s3://cloudvault-file-storage/cloudvault-cicd.pem \
 # ec2-user
 sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/cloudvault-cicd.pem
 sudo chmod 400 /home/ec2-user/.ssh/cloudvault-cicd.pem
-
-# jenkins user
-sudo mkdir -p /var/lib/jenkins/.ssh
-sudo cp /home/ec2-user/.ssh/cloudvault-cicd.pem /var/lib/jenkins/.ssh/cloudvault-cicd.pem
-sudo chown jenkins:jenkins /var/lib/jenkins/.ssh/cloudvault-cicd.pem
-sudo chmod 400 /var/lib/jenkins/.ssh/cloudvault-cicd.pem
