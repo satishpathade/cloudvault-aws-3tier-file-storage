685fb2aec808a76a726649c6d0459c98702519c6
satish <pathadesatish0@gmail.com>
2026-06-22 12:54:02 +0530

    update private key

--- terraform/modules/cicd-server/userdata.sh
+++ terraform/modules/cicd-server/userdata.sh
@@ -7,14 +7,15 @@ ansible-galaxy collection install amazon.aws
 
 
 sudo -u ec2-user git clone https://github.com/satishpathade/cloudvault-aws-3tier-devsecops.git
+
 sudo xfs_growfs -d /
 
 mkdir -p /home/ec2-user/.ssh
 
 aws s3 cp \
-s3://cloudvault-file-storage/cloudvault-cicd.pem \
-/home/ec2-user/.ssh/cloudvault-cicd.pem
+s3://cloudvault-file-storage/CloudVault-CICD.pem \
+/home/ec2-user/.ssh/CloudVault-CICD.pem
 
-# ec2-user
-sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/cloudvault-cicd.pem
-sudo chmod 400 /home/ec2-user/.ssh/cloudvault-cicd.pem
+# ec2-user private key permission
+sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/CloudVault-CICD.pem
+sudo chmod 400 /home/ec2-user/.ssh/CloudVault-CICD.pem
