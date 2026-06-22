7c311873a8f772dad1c77e2ff79f377b2d68e118
satish <pathadesatish0@gmail.com>
2026-06-22 16:18:45 +0530

    change backend key path

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket  = "cloudvault-store-tfstate"
-    key     = "terraform/terraform.tfstate"
+    key     = "env/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
   }
