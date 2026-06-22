a01b2cfba5017eeaa2e0ab6f7f5a3b2d04353468
satish <pathadesatish0@gmail.com>
2026-06-22 16:19:37 +0530

    update cicd userdata script

--- terraform/modules/cicd-server/userdata.sh
+++ terraform/modules/cicd-server/userdata.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 sudo dnf update -y
-sudo dnf install git tree python3-pip ansible -y
+sudo dnf install -y git tree python3-pip ansible
+
 pip3 install boto3 botocore
 ansible-galaxy collection install amazon.aws
 
-
-sudo -u ec2-user git clone https://github.com/satishpathade/cloudvault-aws-3tier-devsecops.git
-
+# Expand root volume
 sudo xfs_growfs -d /
 
+# Clone repository
+
+cd /home/ec2-user
+sudo -u ec2-user git clone https://github.com/satishpathade/cloudvault-aws-3tier-devsecops.git
+
+# Create SSH directory
 mkdir -p /home/ec2-user/.ssh
 
+# Download private key from S3
 aws s3 cp \
 s3://cloudvault-file-storage/CloudVault-CICD.pem \
 /home/ec2-user/.ssh/CloudVault-CICD.pem
 
-# ec2-user private key permission
-sudo chown ec2-user:ec2-user /home/ec2-user/.ssh/CloudVault-CICD.pem
-sudo chmod 400 /home/ec2-user/.ssh/CloudVault-CICD.pem
+# Set ownership and permissions
+chown ec2-user:ec2-user /home/ec2-user/.ssh
+chown ec2-user:ec2-user /home/ec2-user/.ssh/CloudVault-CICD.pem
+
+chmod 700 /home/ec2-user/.ssh
+chmod 400 /home/ec2-user/.ssh/CloudVault-CICD.pem
