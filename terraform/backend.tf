1a4742b83cc68ae8ee654916aeb41e44d0614d48
satish <pathadesatish0@gmail.com>
2026-06-22 12:55:32 +0530

    update backend remote state

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@
 
 terraform {
   backend "s3" {
-    bucket  = "terraform-store-tfstate"
+    bucket  = "cloudvault-store-tfstate"
     key     = "terraform/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
