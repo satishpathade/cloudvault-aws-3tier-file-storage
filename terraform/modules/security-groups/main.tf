8857a4d9baa12af42d58f8d2c221753d4ce6f497
satish <pathadesatish0@gmail.com>
2026-07-15 20:23:37 +0530

    add cicd sg

--- terraform/modules/security-groups/main.tf
+++ terraform/modules/security-groups/main.tf
@@ -22,6 +22,80 @@ resource "aws_security_group" "public_alb" {
   tags = var.tags
 }
 
+
+# CI/CD Security Group
+resource "aws_security_group" "cicd" {
+  name        = "${var.project_name}-cicd-sg"
+  description = "Security group for CI/CD server"
+  vpc_id      = var.vpc_id
+
+  # SSH
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Jenkins
+  ingress {
+    description = "Jenkins"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # SonarQube
+  ingress {
+    description = "SonarQube"
+    from_port   = 9000
+    to_port     = 9000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Grafana
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Kubernetes API
+  ingress {
+    description = "Kubernetes API"
+    from_port   = 6443
+    to_port     = 6443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound Internet
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-cicd-sg"
+  }
+}
+
 # web sg
 resource "aws_security_group" "web" {
   name        = "${var.project_name}-web-sg"
